a91a29c717d32805a022648e0a55c5e9d2b2080c
Arvin He <arvin.he@softtek.com>
2018-04-27 18:22:32 +0800

    测试：文档格式

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,10 @@ git config user.email "vk.he@qq.com"
 git clone "https://${GH_TOKEN}@github.com/aclk/gcr.io_mirror.git"
 
 # get all of the gcr images
-imgs=$(curl -ks 'https://console.cloud.google.com/m/gcr/entities/list'\
-  -H 'cookie: SID=WgX93aiB6sVpD_FPLDBsPHvLnYdhtMXYt9bHsf_TmrmIvLkrnc11D84pIcS-3WB9fYIHKw.; HSID=A--M5SxveLfh2e7Jl; SSID=AqvfThGwBO94ONF2d; OSID=ZAX93cIEBWYq35v3hq6J5U3MNU3voHihnEqmrmIirWBfHluQ3Gjbb4E24vDuPoSVKpC2tg.' -H 'content-type: application/json;charset=UTF-8' --data-binary '["google-containers"]' | grep -P '"' | sed 's/"gcr.ListEntities"//' |cut -d '"' -f2 |sort |uniq)
+imgs=$(curl -ks 'https://console.cloud.google.com/m/gcr/entities/list' \
+    -H 'cookie: SID=WgX93aiB6sVpD_FPLDBsPHvLnYdhtMXYt9bHsf_TmrmIvLkrnc11D84pIcS-3WB9fYIHKw.; HSID=A--M5SxveLfh2e7Jl; SSID=AqvfThGwBO94ONF2d; OSID=ZAX93cIEBWYq35v3hq6J5U3MNU3voHihnEqmrmIirWBfHluQ3Gjbb4E24vDuPoSVKpC2tg.' \
+    -H 'content-type: application/json;charset=UTF-8' \
+    --data-binary '["google-containers"]' | grep -P '"' | sed 's/"gcr.ListEntities"//' |cut -d '"' -f2 |sort |uniq)
 
 # init ant create README.md
 echo -e "Google Container Registry Mirror [last sync $(date +'%Y-%m-%d %H:%M') UTC]\n\
@@ -16,8 +18,8 @@ Total of $(echo ${imgs[@]} | grep -o ' ' | wc -l)'s gcr.io images\n\
 -------\n\n\
 Useage\n\
 -------\n\n\
-\`\`\`bash\ndocker \
-pull gcr.io/google-containers/federation-controller-manager-arm64:v1.3.1-beta.1 \n\
+\`\`\`bash\n\
+docker pull gcr.io/google-containers/federation-controller-manager-arm64:v1.3.1-beta.1 \n\
 # eq \n\
 docker pull abcz/federation-controller-manager-arm64:v1.3.1-beta.1\n\
 \`\`\`\n\n\
@@ -39,12 +41,12 @@ fi
 # remove all of the imgs tmp file
 rm -rf gcr.io_mirror/google_containers/*
 
-for img in ${imgs[@]}  ; do
+for img in ${imgs[@]}; do
     # get all  tags for this image
     gcr_content=$(curl -ks -X GET https://gcr.io/v2/google_containers/${img}/tags/list)
     
     # if this image dir not exits 
-    if [ ! -d gcr.io_mirror/google_containers/${img} ] ; then
+    if [ ! -d gcr.io_mirror/google_containers/${img} ]; then
         mkdir -p gcr.io_mirror/google_containers/${img}
     fi
     
@@ -52,15 +54,16 @@ for img in ${imgs[@]}  ; do
     echo -e "[gcr.io/google-containers/${img}](https://hub.docker.com/r/abcz/${img}/tags/) \n\n----" > gcr.io_mirror/google_containers/${img}/README.md
     
     # create img tmp file,named by tag's name, set access's time,modify's time by this image manifest's timeUploadedMs
-    echo ${gcr_content} | jq -r '.manifest[]|{k: .tag[0],v: .timeUploadedMs} | "touch -amd \"$(date -d @" + .v[0:10] +")\" gcr.io_mirror\/google_containers\/${img}\/"  +.k' | while read i; do
+    echo ${gcr_content} | jq -r '.manifest[]|{k: .tag[0],v: .timeUploadedMs} | "touch -amd \"$(date -d @" + .v[0:10] +")\" gcr.io_mirror\/google_containers\/${img}\/"  +.k' | while read i;
+    do
         eval $i
     done
 
     # get all of the files by last modify time after yesterday,it was new image
     new_tags=$(find ./gcr.io_mirror/google_containers/${img} -path "*.md" -prune -o -mtime -1 -type f -exec basename {} \;)
     
-    for tag in ${new_tags[@]};do
-
+    for tag in ${new_tags[@]};
+    do
         bash ./tags_list.sh abcz/${img}
         # docker pull gcr.io/google-containers/${img}:${tag}
         
